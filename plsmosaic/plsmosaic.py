5119aec22dcfd27ce698eaccbc3ab581a60146eb
kodumen <r.b.daitan@gmail.com>
2016-09-06 23:21:31 +0800

    Upload image to cloudinary
    * Installed package
    * Return upload url

--- plsmosaic/plsmosaic.py
+++ plsmosaic/plsmosaic.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, json
 from urllib.parse import urlparse
-from urllib.request import urlopen
+from urllib.request import urlretrieve
+import cloudinary.uploader
 
 app = Flask(__name__)
 
@@ -17,11 +18,13 @@ def command():
     if url_info.scheme not in ('http', 'https'):
         return 'Invalid protocol. Allowed: HTTP, HTTPS.'
 
-    image = urlopen(request.form['text'])
-    if not image.info().get_content_type().startswith('image/'):
+    tmp_file, image_info = urlretrieve(request.form['text'])
+    if not image_info.get_content_type().startswith('image/'):
         return 'Invalid content type. Allowed: image/*'
+
+    upload_info = cloudinary.uploader.upload(tmp_file)
 
     return json.jsonify({
         'response_type': 'in_channel',
-        'text': request.form['text']
+        'text': upload_info['secure_url']
         })
