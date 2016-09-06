dcbe8724fc7bd9e457bf9f8111bc65cfb4717a22
kodumen <r.b.daitan@gmail.com>
2016-09-06 21:40:22 +0800

    Return the image link in channel

--- plsmosaic/plsmosaic.py
+++ plsmosaic/plsmosaic.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request, json
 
 app = Flask(__name__)
 
 @app.route('/')
 def version():
     return 'pls mosaic 0.0.1a';
+
+@app.route('/', methods=['POST'])
+def command():
+    if request.form['text'] == '':
+        return 'pls mosaic 0.0.1a'
+
+    return json.jsonify({
+        'response_type': 'in_channel',
+        'text': request.form['text']
+        })
